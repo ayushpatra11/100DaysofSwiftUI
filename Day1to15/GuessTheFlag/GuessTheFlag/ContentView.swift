3a5260b273c84bd40178b982ae464cb8ba2fd20c
ayushpatra11 <ayushpatra11@gmail.com>
2024-05-22 01:28:58 +0530

    Completed game completion logic. Enhanced UI as per basic understanding.

--- Day1to15/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day1to15/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,16 +12,33 @@ struct ContentView: View {
     
     @State var score:Int = 0
     
+    @State var isGameStarted:Bool = false
+    @State var isGameStopped:Bool = false
+    
+    func startGame(){
+        isGameStarted = true
+        isGameStopped = false
+    }
+    
+    func resetGame(){
+        score = 0
+        isGameStarted = false
+        isGameStopped = true
+    }
+    
     func updatedScore(num: Int){
         if(correctAnswer == num){
             score+=1
         }
-        else{
-            score-=1
-        }
         countries.shuffle()
         correctAnswer = Int.random(in: 0..<3)
+        
+        if(score == 10){
+            isGameStarted = false
+            isGameStopped = true
+        }
     }
+
     
     @State var correctAnswer:Int = Int.random(in: 0..<3)
     var body: some View {
@@ -29,41 +46,75 @@ struct ContentView: View {
             
             //COLOR:
             
-            Color.blue
+            LinearGradient(colors: [.indigo, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
             
-            VStack(spacing: 20){
-                
+            
+            VStack{
+                //TITLE
                 Text("GuessTheFlag")
-                    .font(.title)
+                    .font(.system(size: 40))
                     .fontWeight(.bold)
                     .foregroundColor(Color.orange)
-                    
                 
-                Text("Your Current Score: \(score)")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                
-                Text("Which of the following is the flag of: \(countries[correctAnswer])")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                
-                ForEach(0..<3){
-                    number in
-                    Button{
-                        //Button Was tapped
-                        updatedScore(num: number)
-                    }label: {
-                        Image("\(countries[number])")
+                if(isGameStarted == false){
+                    Group {
+                        Button{
+                            startGame()
+                        }label: {
+                            Text("Start Game")
+                        }.alert("Game Over", isPresented: $isGameStopped) {
+                            Button("Retry") {
+                                // Logic to restart the game
+                                resetGame()
+                            }
+                            Button("Exit", role: .cancel) { }
+                    }message: {
+                        Text("You have won the game.")
                     }
+                    }
+                }else{
+                    VStack{
+                        
+                        Text("Your Current Score: \(score)")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                        
+                        Text("Choose the correct flag: \n")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.center)
+                        
+                        Text("\(countries[correctAnswer])")
+                            .font(.system(size: 38))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        
+                        
+                        ForEach(0..<3){
+                            number in
+                            Button{
+                                //Button Was tapped
+                                updatedScore(num: number)
+                            }label: {
+                                Image("\(countries[number])")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                                    .shadow(radius: 10)
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 10)
                 }
-                
-                Spacer()
             }
-            .padding(.top, 100)
+            .padding(.top, 70)
+            .padding(.bottom, 50)
         }
         .ignoresSafeArea()
     }
