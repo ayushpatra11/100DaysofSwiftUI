c02d2fadcb1d05c78ff463e501d33f47ccf7a286
ayushpatra11 <ayushpatra11@gmail.com>
2024-05-24 01:58:13 +0530

    Setting Up Next Project
    This will be the next project to understand views and modifiers.

---
+++ Day1to15/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ayush Patra on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
